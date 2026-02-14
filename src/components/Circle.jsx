6d4ad812c9dbc21ce1051f1430d4f1a32b2996bb
Israel Oyedele <israeloyedele01@gmail.com>
2026-02-14 11:18:54 -0500

    Add New Component

---
+++ src/components/Circle.jsx
@@ -0,0 +1,11 @@
+import {COLORS, PICK} from "../utils/constants.js";
+
+export function Circle(props) {
+    const { color, icon, pick } = props;
+
+    return (
+        <div onClick={pick} className="circle" style={{ "--circle-color": color }}>
+            <img src={icon} alt=""  />
+        </div>
+    )
+}
