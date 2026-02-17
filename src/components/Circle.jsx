5b25af79311307761df24c05be59558f3cc1066d
Israel Oyedele <israeloyedele01@gmail.com>
2026-02-17 08:05:04 -0500

    Advanced Mode Added

--- src/components/Circle.jsx
+++ src/components/Circle.jsx
@@ -1,15 +1,16 @@
 import { motion } from "framer-motion"
 
 export function Circle(props) {
-    const { isButton, color, icon, pick } = props;
+    const { className, isButton, color, icon, pick } = props;
 
     return (
         <motion.div
             initial={!isButton && {opacity: 0, scale: 0}}
             whileHover={isButton && {scale: 1.1, transition: {duration: 0.3}}}
             animate={!isButton && {opacity: 1, scale: 1.1, transition: {duration: 0.5}}}
-            onClick={pick} className="circle"
-             style={{ "--circle-color": color }}>
+            onClick={pick}
+            className={`circle ${className}`}
+            style={{ "--circle-color": color }}>
             <img src={icon} alt=""  />
         </motion.div>
     )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { Score } from "./Score.jsx";
 import logo from "../assets/images/logo.svg"
 import advancedLogo from "../assets/images/logo-bonus.svg"
+import { easeOut, motion } from "framer-motion";
 
 export function Header(props) {
     const { outcome, gameMode, score } = props;
@@ -10,8 +11,11 @@ export function Header(props) {
                 <img id="logo" src={gameMode? logo : advancedLogo} alt="img" />
                 <Score score={score} />
             </div>
-            <p className="outcome">{outcome}</p>
-
+            {outcome && <motion.p
+                initial={{opacity: 0, scale: 0}}
+                animate={{opacity: 1, scale: 1, transition: { duration: 0.5, ease: easeOut }}}
+                className="outcome">{outcome}</motion.p>
+            }
         </div>
 
 
